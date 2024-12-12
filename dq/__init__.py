088d5221545498ed58f3f6f8574e04b1b007947f
Mateusz <mateusz.bialek@algolytics.pl>
2024-12-12 12:16:09 +0100

    Bump version: 0.5.0 → 0.6.0

--- dq/__init__.py
+++ dq/__init__.py
@@ -5,6 +5,6 @@ from .job import JobConfig
 
 __author__ = """Algolytics Technologies"""
 __email__ = 'info@algolytics.pl'
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 
 __all__ = ['DQClient', 'JobConfig']
